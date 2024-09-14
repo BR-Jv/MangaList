de52b3a2153f2bbbe24b6989f19ab8da8df1a53d
João.Victor <jv.alves193@gmail.com>
2024-09-14 00:03:03 -0300

    add models autor

--- mangalist/admin.py
+++ mangalist/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 from .models import * 
 
+@admin.register(Manga)
+class MangaAdmin(admin.ModelAdmin):
+    list_display = ['nome',]
+    list_display_links = ['nome']
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    list_display_links = ['nome']
+
+
+@admin.register(Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    list_display_links = ['nome']
 
 @admin.register(Autor)
-class AutorAdmin(admin.ModelAdmin): 
-    list_display = ['first_name', 'last_name']
-    list_display_links = ['first_name']
+class AutorAdmin(admin.ModelAdmin):
+    pass
--- mangalist/models.py
+++ mangalist/models.py
@@ -1,36 +1,75 @@
 from django.db import models
 
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=75)
-    last_name = models.CharField(max_length=75)
+class Ilustrador(models.Model):
+    first_name = models.CharField(max_length=95, null=False, blank=False)
+    last_name = models.CharField(max_length=95, null=True, blank=True)
+    
+    class Meta: 
+        verbose_name = 'Ilustrador'
+        verbose_name_plural = "Ilustradores"
     
     def fullName(self):
         return f'{self.first_name} {self.last_name}'
     
-    class Meta: 
-        abstract = True
+    def __str__(self) -> str:
+        return self.fullName()
 
-class Autor(Person): 
+class Autor(models.Model):
+    first_name = models.CharField(max_length=95, null=False, blank=False)
+    last_name = models.CharField(max_length=95, null=True, blank=True)
+    
+    class Meta: 
+        verbose_name = 'Autor'
+        verbose_name_plural = "Autores"
+    
+    def fullName(self):
+        return f'{self.first_name} {self.last_name}'
     
     def __str__(self) -> str:
         return self.fullName()
 
-class Ilustrador(Person):
-    first_name = models.CharField(max_length=75, null=True, blank=True)
-    last_name = models.CharField(max_length=75, null=True, blank=True)
-     
-    def __str__(self) -> str:
-        return self.fullName()
+STATUS = (
+    ('l', 'Em lançamento'),
+    ('h', 'Em hiato'),
+    ('f', 'Finalizado'),
+)
 
 class Manga(models.Model):
-    pass 
+    nome = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    volumes = models.IntegerField(null=True, blank=True)
+    capitulos = models.IntegerField(null=True, blank=True)
+    status = models.CharField(max_length=1, choices=STATUS, default='f') 
+    data_lancamento = models.DateField(null=False, blank=False) 
+    data_encerramento = models.DateField()
+    autor = models.ForeignKey('Autor', on_delete=models.DO_NOTHING, related_name='mangas')
+
+
+    class Meta: 
+        verbose_name = 'Mangá'
+        verbose_name_plural = "Mangás"
+
+    def __str__(self) -> str:
+        return self.nome 
+
 
 class Categoria(models.Model):
-    pass 
+    nome = models.CharField(max_length=55, unique=True, null=False, blank=False)
 
-class Status(models.Model):
-    pass 
+    class Meta: 
+        verbose_name = 'Categoria'
+        verbose_name_plural = "Categorias"
+
+    def __str__(self) -> str:
+        return self.nome
+
 
 class Genero(models.Model):
-    pass 
+    nome = models.CharField(max_length=55, unique=True, null=False, blank=False)
+
+    class Meta: 
+        verbose_name = 'Genero'
+        verbose_name_plural = "Generos"
+
+    def __str__(self) -> str:
+        return self.nome
